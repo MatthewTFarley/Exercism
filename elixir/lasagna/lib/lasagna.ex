2054824b33f4969ff71d679f2915b9d829ae729b
Matthew Farley <matthewthomasfarley@gmail.com>
2022-01-20 19:15:17 -0500

    Add elixir lasagna solution

---
+++ elixir/lasagna/lib/lasagna.ex
@@ -0,0 +1,23 @@
+defmodule Lasagna do
+  @expected_minutes_in_oven 40
+
+  def expected_minutes_in_oven do
+    @expected_minutes_in_oven
+  end
+
+  def remaining_minutes_in_oven(minutes) do
+    expected_minutes_in_oven() - minutes
+  end
+
+  def preparation_time_in_minutes(layers) do
+    layers * 2
+  end
+
+  def total_time_in_minutes(layers, minutes) do
+    preparation_time_in_minutes(layers) + minutes
+  end
+
+  def alarm do
+    "Ding!"
+  end
+end
